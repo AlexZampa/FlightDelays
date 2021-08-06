7764e01dc52e06b4b06b444d952fbe7bda2dea92
admin <admin@192.168.234.129>
2021-08-06 18:27:49 +0200

    Esercizio completato

--- src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
+++ src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
@@ -7,10 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.extflightdelays.model.Airline;
 import it.polito.tdp.extflightdelays.model.Airport;
 import it.polito.tdp.extflightdelays.model.Flight;
+import it.polito.tdp.extflightdelays.model.Rotta;
 
 public class ExtFlightDelaysDAO {
 
@@ -37,24 +39,25 @@ public class ExtFlightDelaysDAO {
 		}
 	}
 
-	public List<Airport> loadAllAirports() {
+	
+	
+	public void loadAllAirports(Map<Integer, Airport> idMap) {
 		String sql = "SELECT * FROM airports";
-		List<Airport> result = new ArrayList<Airport>();
-
 		try {
 			Connection conn = ConnectDB.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
-				Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
-						rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
-						rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
-				result.add(airport);
+				if(!idMap.containsKey(rs.getInt("ID"))) {
+					Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
+							rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
+							rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
+					idMap.put(airport.getId(), airport);
+				}
 			}
 
 			conn.close();
-			return result;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -63,6 +66,8 @@ public class ExtFlightDelaysDAO {
 		}
 	}
 
+	
+	
 	public List<Flight> loadAllFlights() {
 		String sql = "SELECT * FROM flights";
 		List<Flight> result = new LinkedList<Flight>();
@@ -91,4 +96,69 @@ public class ExtFlightDelaysDAO {
 			throw new RuntimeException("Error Connection Database");
 		}
 	}
+	
+	
+	
+	public int getAirlinesNumber(Airport airport) {
+		String sql = "SELECT  COUNT(DISTINCT airline_id) AS tot "
+				+ "FROM flights "
+				+ "WHERE origin_airport_id = ? OR destination_airport_id = ?";
+		int result = -1;
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, airport.getId());
+			st.setInt(2, airport.getId());
+			ResultSet res = st.executeQuery();
+			
+			if(res.next()) {
+				result = res.getInt("tot");
+			}
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
+		
+		return result;
+	}
+
+
+
+	public List<Rotta> getRotte(Map<Integer, Airport> idMap) {
+		String sql = "SELECT origin_airport_id, destination_airport_id, COUNT(*) AS tot "
+				+ "FROM flights "
+				+ "GROUP BY origin_airport_id, destination_airport_id";
+		List<Rotta> result = new ArrayList<>();
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			
+			while(res.next()) {
+				Airport sorgente = idMap.get(res.getInt("origin_airport_id"));
+				Airport destinazione = idMap.get(res.getInt("destination_airport_id"));
+				
+				if(sorgente != null && destinazione != null) {
+					result.add(new Rotta(sorgente, destinazione, res.getInt("tot")));
+				}
+				else {
+					System.out.println("Errore in getRotte");
+				}
+			}
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
+		
+		return result;
+	}
+	
+	
+	
 }
--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -1,5 +1,131 @@
 package it.polito.tdp.extflightdelays.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import it.polito.tdp.extflightdelays.db.ExtFlightDelaysDAO;
+
+
 public class Model {
 
+	private SimpleWeightedGraph<Airport, DefaultWeightedEdge> grafo;
+	private Map<Integer, Airport> idMap; 
+	private ExtFlightDelaysDAO dao;
+	private Map<Airport, Airport> visita;
+	
+	public Model() {
+		this.idMap = new HashMap<Integer, Airport>();
+		this.dao = new ExtFlightDelaysDAO();
+		this.dao.loadAllAirports(this.idMap);
+		this.visita = new HashMap<Airport, Airport>();
+	}
+	
+	
+	public void creaGrafo(int n_compagnie) {
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		// Aggiungo vertici
+		for(Airport a: this.idMap.values()) {
+			if(this.dao.getAirlinesNumber(a) > n_compagnie) {
+				this.grafo.addVertex(a);
+			}
+			
+			for(Rotta r : dao.getRotte(this.idMap)) {
+				if(this.grafo.containsVertex(r.getA1()) && this.grafo.containsVertex(r.getA2())) {
+					DefaultWeightedEdge edge = this.grafo.getEdge(r.getA1(), r.getA2());
+					
+					if(edge == null) {
+						Graphs.addEdgeWithVertices(this.grafo, r.getA1(), r.getA2(), r.getPeso());
+					}
+					else {
+						double pesoVecchio = this.grafo.getEdgeWeight(edge);
+						double pesoNuovo = pesoVecchio + r.getPeso();
+						this.grafo.setEdgeWeight(edge, pesoNuovo);
+					}
+				}
+			}
+			
+		}	
+	}
+	
+	
+	public int vertexNumber() {
+		return this.grafo.vertexSet().size();
+	}
+	
+	public int edgeNumber() {
+		return this.grafo.edgeSet().size();
+	}
+	
+	public Collection<Airport> getAirport() {
+		return this.grafo.vertexSet();
+	}
+	
+	public List<Airport> trovaPercorso(Airport a1, Airport a2) {
+		List<Airport> percorso = new ArrayList<Airport>();
+		BreadthFirstIterator<Airport, DefaultWeightedEdge> iter = new BreadthFirstIterator<>(this.grafo,a1);
+		// aggiungo subito radice dell'albero
+		visita.put(a1, null);
+		
+		iter.addTraversalListener(new TraversalListener<Airport, DefaultWeightedEdge>() {
+			
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Airport> e) {}
+			
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Airport> e) {}
+			
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				Airport sorgente = grafo.getEdgeSource(e.getEdge());
+				Airport destinazione = grafo.getEdgeTarget(e.getEdge());
+				
+				if(!visita.containsKey(destinazione) && visita.containsKey(sorgente)) {
+					 visita.put(destinazione, sorgente);
+				}
+				else if (!visita.containsKey(sorgente) && visita.containsKey(destinazione)) {
+					visita.put(sorgente, destinazione);
+				}
+				
+			}
+			
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {}
+			
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {}
+		});
+		
+		
+		while(iter.hasNext()) {
+			iter.next();
+		}
+		
+		if(!visita.containsKey(a1) || !visita.containsKey(a2)) {
+			// i due aeroporti non sono collegati
+			return null;
+		}
+		
+		Airport step = a2;
+		while(!step.equals(a1)) {
+			percorso.add(step);
+			step = visita.get(step);
+		}
+		
+		percorso.add(a1);
+		return percorso;
+	}
+	
 }
